0c3008976e7877152f4c16585d72a1364183d853
Wiktor Kabanow <wiktor.kabanow@develey.pl>
2024-05-14 15:08:28 +0200

    Static configuration fields in ConnectionPool (pool size and conn lifetime)

--- src/main/java/krystal/framework/database/implementation/ConnectionPool.java
+++ src/main/java/krystal/framework/database/implementation/ConnectionPool.java
@@ -7,11 +7,13 @@ import krystal.framework.database.abstraction.ProviderInterface;
 import krystal.framework.database.abstraction.QueryExecutorInterface;
 import krystal.framework.logging.LoggingInterface;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.val;
 import org.springframework.stereotype.Service;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.Duration;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +27,21 @@ public class ConnectionPool implements ConnectionPoolInterface, LoggingInterface
 	
 	private @Getter final Map<ProviderInterface, HikariDataSource> pools;
 	private final QueryExecutorInterface queryExecutor;
+	
+	/**
+	 * Set the maximum connection pool size for particular {@link ProviderInterface} by adding to this collection.
+	 */
+	private static @Getter final Map<ProviderInterface, Integer> maximumPoolSizes = new HashMap<>();
+	
+	/**
+	 * Default value if not specified within {@link #getMaximumPoolSizes()}. Set or 3 by default.
+	 */
+	private static @Getter @Setter int defaultMaximumPoolSize = 3;
+	
+	/**
+	 * Default lifetime for a connection as defined in {@link HikariDataSource#getMaxLifetime()}.
+	 */
+	private static @Getter @Setter Duration defaultConnectionLifeTime;
 	
 	ConnectionPool(QueryExecutorInterface queryExecutor) {
 		this.queryExecutor = queryExecutor;
@@ -48,9 +65,10 @@ public class ConnectionPool implements ConnectionPoolInterface, LoggingInterface
 		val config = new HikariConfig();
 		config.setJdbcUrl(queryExecutor.getConnectionStrings().get(provider));
 		queryExecutor.getConnectionProperties().get(provider).forEach((p, v) -> config.addDataSourceProperty(p.toString(), v));
-		config.setMaximumPoolSize(3);
+		
+		config.setMaximumPoolSize(Optional.ofNullable(maximumPoolSizes.get(provider)).orElse(defaultMaximumPoolSize));
 		config.setPoolName(provider.name() + " connection pool");
-		// config.setMaxLifetime(60000);
+		Optional.ofNullable(defaultConnectionLifeTime).ifPresent(duration -> config.setMaxLifetime(duration.toMillis()));
 		
 		val dataSource = new HikariDataSource(config);
 		pools.put(provider, dataSource);
