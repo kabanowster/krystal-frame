861bd9bb9eb3c66833d0bf590d94a08ffa047a71
Wiktor Kabanow <wiktor.kabanow@develey.pl>
2024-07-07 12:52:20 +0200

    Arguments control methods patches; Docs;

--- src/main/java/krystal/framework/commander/CommanderInterface.java
+++ src/main/java/krystal/framework/commander/CommanderInterface.java
@@ -1,6 +1,8 @@
 package krystal.framework.commander;
 
+import com.google.common.base.Strings;
 import com.google.common.io.Files;
+import krystal.Tools;
 import krystal.framework.KrystalFramework;
 import krystal.framework.logging.LoggingInterface;
 import lombok.NonNull;
@@ -11,6 +13,7 @@ import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -18,11 +21,27 @@ import java.util.stream.Stream;
 /**
  * To read commands from outside of app, use {@link #readCommandsFromTextFile(File)}, which can be scheduled with your flow manager. Commands can be defined with {@link CommandInterface} as enums and parsed through
  * {@link #executeCommand(CommandInterface, List)}.
+ * <p>
+ * <h3>Commands parsing patterns</h3>
+ * <ul>
+ *     <li>The first word separated by any number of {@code space} chars is a command, parsable as {@link CommandInterface}.</li>
+ *     <li>Strings following the command are recognised as argument-value pairs. The spacing <strong>does not</strong> influence the pattern recognition.</li>
+ *     <li>Unmatched pairs, null, quoted or bracketed, and the {@code --} arg - all are treated as <i><b>empty</b></i> arguments (they hold no assigned value information, will return {@code null}).</li>
+ *     <li>Value is assigned using any number of following characters: {@code space}, {@code =} or {@code :}.</li>
+ *     <li>Enquoted args do not permit quote characters within (they close the pattern).</li>
+ *     <li>Bracketed args allow of usage any character within, including other brackets (the last bracket closes the pattern).</li>
+ *     <li>The {@code --} arg escapes any pattern parsing for any string put after.</li>
+ *     <li>Single and double quotes, different brackets - are not distinguished.</li>
+ *     <li>{@code -} and {@code --} as prefixes are treated with no difference. Args get stripped from these when parsing. Using dashes for arguments make the parse error-proof and values assignments clear.</li>
+ * </ul>
  *
  * @see #parseCommand(String)
- * @see #getValueIfArgumentIs(String, String...)
+ * @see #getValueIfArgumentMatches(String, String...)
+ * @see #getValueIfArgumentIsEnclosed(String, EnclosingType)
  * @see #argumentMatches(String, String...)
+ * @see #argumentIsEnclosed(String, EnclosingType)
  * @see #getArgumentValue(String)
+ * @see #getEnclosedValue(String)
  */
 public interface CommanderInterface extends LoggingInterface {
 	
@@ -39,6 +58,8 @@ public interface CommanderInterface extends LoggingInterface {
 	
 	/**
 	 * Implement the execution of given command and arguments. Using switch with enum implementing {@link CommandInterface} is advised.
+	 *
+	 * @return Boolean - true if the command was correctly parsed (irrespectively to the outcome), false if not found or errored. Susceptible to custom interpretation.
 	 */
 	boolean executeCommand(CommandInterface command, List<String> arguments);
 	
@@ -61,17 +82,28 @@ public interface CommanderInterface extends LoggingInterface {
 	 * First trimmed word is a command, case-insensitive, followed by arguments with "--" or "-".
 	 */
 	default boolean parseCommand(String command) {
-		val line = command.split(" ", 2);
+		val line = command.split(" +", 2);
 		return executeCommand(
 				() -> line[0].strip().toLowerCase(),
-				line.length > 1 ? Stream.of(line[1].split("--|\\s(?=-[a-zA-Z])")).map(String::strip).filter(s -> !s.isEmpty()).toList() : List.of()
+				line.length > 1 ? getArguments(line[1]) : List.of()
 		);
 	}
 	
 	/**
-	 * If provided argument (String) matches pattern or any of the variants, returns its value.
+	 * Use pattern recognition regex to split the provided line of arguments into argument-value pairs or single arguments/values.
 	 */
-	default Optional<String> getValueIfArgumentIs(String argument, String... variants) {
+	static List<String> getArguments(String argumentsLine) {
+		return Stream.of(argumentsLine.splitWithDelimiters("([\"'].+[\"'])|([[({<].+[\\\\])}>])|(-{1,2}\\w+(-\\w+)*[ =:]+\\w*)|(-- .*)", 0))
+		             .map(String::strip)
+		             .map(s -> s.replaceAll("^-{1,2}(?=.*?)", ""))
+		             .filter(s -> !Strings.isNullOrEmpty(s))
+		             .toList();
+	}
+	
+	/**
+	 * Return the value if argument matches any of the provided variants.
+	 */
+	static Optional<String> getValueIfArgumentMatches(String argument, String... variants) {
 		if (argumentMatches(argument, variants)) {
 			return Optional.ofNullable(getArgumentValue(argument));
 		} else {
@@ -80,19 +112,73 @@ public interface CommanderInterface extends LoggingInterface {
 	}
 	
 	/**
+	 * Return the value if argument is enclosed in {@link EnclosingType}.
+	 */
+	static Optional<String> getValueIfArgumentIsEnclosed(String argument, EnclosingType within) {
+		if (argumentIsEnclosed(argument, within)) {
+			return Optional.ofNullable(getEnclosedValue(argument));
+		} else {
+			return Optional.empty();
+		}
+	}
+	
+	/**
 	 * Checks if argument fits the valid pattern, and if it's name is one of the variants.
 	 */
-	default boolean argumentMatches(@Nullable String argument, String... variants) {
+	static boolean argumentMatches(@Nullable String argument, String... variants) {
 		if (argument == null) return false;
-		val regex = "^(%s)([\\s=]\\w[\\w\\W]*?)?$".formatted(String.join("|", variants));
+		val regex = "^(%s)([ =:]+.*?)?$".formatted(String.join("|", Arrays.stream(variants).map(Tools::escapeRegexSpecials).toList()));
+		return argument.matches(regex);
+	}
+	
+	static boolean argumentValid(@Nullable String argument) {
+		if (argument == null) return false;
+		return argument.matches("\\w+(-\\w+)*[ =:]+\\w*");
+	}
+	
+	/**
+	 * Checks if the argument is enquoted or bracketed value.
+	 */
+	static boolean argumentIsEnclosed(@Nullable String argument, EnclosingType within) {
+		if (argument == null) return false;
+		val regex = switch (within) {
+			case QUOTES -> "^[\"'].+?[\"']$";
+			case BRACKETS -> "^[[(<{].+?[\\\\])>}]$";
+		};
 		return argument.matches(regex);
 	}
 	
 	/**
-	 * Gets the value of the argument, issued with either " " or "=".
+	 * Gets the value of the argument, issued with either {@code " "}, {@code "="} or {@code ":"}, or returns {@code null}.
 	 */
-	default String getArgumentValue(@NonNull String argument) {
-		return argument.split("[\\s=]", 2)[1].strip().transform(s -> s.isEmpty() ? null : s);
+	static @Nullable String getArgumentValue(@NonNull String argument) {
+		try {
+			return argument.split("[ =:]+", 2)[1].strip().transform(s -> s.isEmpty() ? null : s);
+		} catch (IndexOutOfBoundsException _) {
+			return null;
+		}
+	}
+	
+	/**
+	 * Return argument's name.
+	 */
+	static String getArgumentName(@NonNull String argument) {
+		return argument.split("[ =:]+", 2)[0];
+	}
+	
+	/**
+	 * Gets the value of the enclosed argument. Effectively substring between first and last character.
+	 */
+	static @Nullable String getEnclosedValue(@NonNull String argument) {
+		try {
+			return argument.substring(1, argument.length() - 1);
+		} catch (IndexOutOfBoundsException _) {
+			return null;
+		}
+	}
+	
+	enum EnclosingType {
+		QUOTES, BRACKETS
 	}
 	
 }
