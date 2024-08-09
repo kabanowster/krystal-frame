a884d204e7009994c6112aa1a84b75f0f190796a
Wiktor Kabanow <wiktor.kabanow@develey.pl>
2024-08-09 14:01:28 +0200

    Added default persistence-specific builder methods;

--- src/main/java/krystal/framework/tomcat/KrystalServlet.java
+++ src/main/java/krystal/framework/tomcat/KrystalServlet.java
@@ -5,21 +5,25 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import krystal.JSON;
 import krystal.VirtualPromise;
+import krystal.framework.database.persistence.PersistenceInterface;
 import krystal.framework.logging.LoggingInterface;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Singular;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
+import org.json.JSONObject;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Map;
-import java.util.Vector;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Builder of asynchronous (non-blocking) {@link HttpServlet}, utilising Virtual Threads. Remember to define {@link #mappings}.
@@ -35,6 +39,7 @@ import java.util.function.Consumer;
  *
  * @see VirtualPromise
  */
+@Log4j2
 @Builder
 @WebServlet(asyncSupported = true)
 public class KrystalServlet extends HttpServlet implements LoggingInterface {
@@ -42,7 +47,7 @@ public class KrystalServlet extends HttpServlet implements LoggingInterface {
 	/**
 	 * @see KrystalServlet
 	 */
-	private @Singular @Getter List<String> mappings;
+	private @Singular @Getter Set<String> mappings;
 	/**
 	 * @see KrystalServlet
 	 */
@@ -69,6 +74,125 @@ public class KrystalServlet extends HttpServlet implements LoggingInterface {
 	 * @see KrystalServlet
 	 */
 	private Consumer<KrystalServlet> destroy;
+	
+	
+	/*
+	 * Builder expansion for persistence methods
+	 */
+	
+	public static KrystalServlet getPersistenceServlet(String context, String name, Set<PersistenceMappingInterface> mappings) {
+		return KrystalServlet.builder()
+		                     .servletName(name)
+		                     .servletContextString(context)
+		                     .addPersistenceMappings(mappings)
+		                     .serveGetPersistenceMappings(mappings)
+		                     .servePutPersistenceMappings(mappings)
+		                     .build();
+	}
+	
+	public static class KrystalServletBuilder {
+		
+		public KrystalServletBuilder addPersistenceMappings(Collection<PersistenceMappingInterface> mappings) {
+			val builderMappings = Optional.ofNullable(this.mappings).orElseGet(() -> {
+				this.mappings = new ArrayList<>();
+				return this.mappings;
+			});
+			mappings.forEach(mapping -> {
+				builderMappings.add(mapping.mapping());
+				builderMappings.add(mapping.single());
+				builderMappings.add(mapping.plural());
+			});
+			return this;
+		}
+		
+		public KrystalServletBuilder servePutPersistenceMappings(Collection<PersistenceMappingInterface> mappings) {
+			return this.servePut(
+					(req, resp) -> VirtualPromise.supply(() -> new RequestInfo(req, mappings))
+					                             .accept(info -> {
+						                             if (!info.patternIsMapping) {
+							                             // pattern is plural or with matching-value ("/*")
+							                             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+							                             return;
+						                             }
+						                             try {
+							                             val body = req.getReader().lines().collect(Collectors.joining());
+							                             if (JSON.into(body, info.mapping.getPersistenceClass()) instanceof PersistenceInterface obj) obj.save();
+						                             } catch (IOException e) {
+							                             log.error(e);
+							                             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+						                             }
+					                             }));
+		}
+		
+		public KrystalServletBuilder serveGetPersistenceMappings(Collection<PersistenceMappingInterface> mappings) {
+			return this.serveGet(
+					(req, resp) -> VirtualPromise.supply(() -> new RequestInfo(req, mappings))
+					                             .accept(info -> {
+						                             resp.setContentType("application/json");
+						                             if (info.patternIsMapping) {
+							                             // pattern is singular with no matching-value
+							                             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+							                             return;
+						                             }
+						                             if (info.patternIsPlural) {
+							                             // TODO parameters as filters
+							                             PersistenceInterface.promiseAll(info.mapping.getPersistenceClass())
+							                                                 .map(Stream::toList)
+							                                                 .map(JSON::from)
+							                                                 .map(JSONObject::toString)
+							                                                 .accept(result -> {
+								                                                 try {
+									                                                 resp.getWriter().write(result);
+								                                                 } catch (IOException e) {
+									                                                 log.error(e);
+									                                                 resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+								                                                 }
+							                                                 })
+							                                                 .joinThrow();
+						                             } else {
+							                             try {
+								                             val id = req.getHttpServletMapping().getMatchValue();
+								                             val result = info.mapping.getPersistenceClass().getDeclaredConstructor(String.class).newInstance(id);
+								                             if (result.noneIsNull())
+									                             resp.getWriter().write(result.toJSON().toString());
+								                             else resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+								                             
+							                             } catch (NumberFormatException e) {
+								                             log.error(e);
+								                             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+							                             } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException | IOException e) {
+								                             log.error(e);
+								                             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+							                             }
+						                             }
+					                             }));
+		}
+		
+		private static class RequestInfo {
+			
+			private final PersistenceMappingInterface mapping;
+			private final boolean patternIsPlural;
+			private final boolean patternIsMapping;
+			
+			public RequestInfo(HttpServletRequest request, Collection<PersistenceMappingInterface> mappings) {
+				String pattern = request.getHttpServletMapping().getPattern();
+				patternIsPlural = pattern.matches("^/\\w+s$");
+				patternIsMapping = !pattern.matches("^/\\w+(s|\\W+)$");
+				val patternName = pattern.splitWithDelimiters("(?<=/)\\w+[^s\\W]", 0)[1].toLowerCase();
+				mapping = mappings.stream()
+				                  .filter(m -> m.name().equals(patternName))
+				                  .findAny()
+				                  .orElseThrow(NoSuchElementException::new);
+			}
+			
+		}
+		
+	}
+	
+	
+	/*
+	 * HttpServlet-specific overrides
+	 */
 	
 	@Override
 	public void init() throws ServletException {
